3ed3e198c7da8dbcf14a64e086cc2bbe22b9031a
aryanpuransanaye <aryanpuransanayeh@gmail.com>
2025-06-22 10:17:03 +0330

    feat: added reply and inline keyboards with handlers for mood, modd history, bot info, and creator sections

---
+++ telegram/bot.py
@@ -0,0 +1,73 @@
+import telebot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+from decouple import config
+
+class Bot:
+
+    def __init__(self, token):
+
+        self.bot = telebot.TeleBot(token)
+        
+        self.menu_buttons_name = ['Choose your Mood', 'Bot Info', 'mood history', 'Creator']
+        self.mood_buttons_name = ['😁 Happy', '😞 Sad', '😪 Tired', '😰 Stressed', '🥰 In Love']
+        self.creator_buttons_name = {'GitHub':'https://github.com/aryanpuransanaye', 'Linkdin':'https://www.linkedin.com/in/aryan-puransanaye/'}
+
+        self.setup_handlers()
+
+    def send_main_menu(self, message):
+
+        keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        keyboard.add(*[KeyboardButton(name) for name in self.menu_buttons_name])
+
+        self.bot.send_message(message.chat.id, 'Choos an option below: ', reply_markup=keyboard)
+
+    def send_mood_menu(self, message):
+
+        keyboard = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        keyboard.add(*[KeyboardButton(name) for name in self.mood_buttons_name])
+        keyboard.add( '🔙 Back to Main Menu')
+
+        self.bot.send_message(message.chat.id, 'Choos your mood: ', reply_markup=keyboard)
+
+    def send_creator_menu(self, message):
+       
+        inline_keyboard = InlineKeyboardMarkup(row_width=2)
+        inline_keyboard.add(*[InlineKeyboardButton(text = key, url = value) for key, value in self.creator_buttons_name.items()])
+
+        from text_message import creator_text
+        self.bot.send_message(message.chat.id, creator_text, reply_markup=inline_keyboard)
+
+    def setup_handlers(self):
+
+        @self.bot.message_handler(commands=['start'])
+        def start_handler(message):
+            self.send_main_menu(message)
+
+        @self.bot.message_handler(func=lambda m: m.text == 'Choose your Mood')
+        def mood_menu_handler(message):
+            self.send_mood_menu(message)
+
+        @self.bot.message_handler(func=lambda m:m.text == 'Bot Info')
+        def info_message_handler(message):
+            from text_message import info_text
+            self.bot.send_message(message.chat.id, info_text)
+
+        @self.bot.message_handler(func=lambda m:m.text == 'Creator')
+        def creator_menu_handler(message):
+            self.send_creator_menu(message)
+
+        @self.bot.message_handler(func=lambda m:m.text == '🔙 Back to Main Menu')
+        def main_menu_handler(message):
+            self.send_main_menu(message)
+
+    def run(self):
+        self.bot.polling()
+
+
+if __name__ == '__main__':
+
+    
+    TOKEN = config('TELEGRAM_BOT_TOKEN')
+    mood_bot = Bot(TOKEN)
+    mood_bot.run()
+
---
+++ telegram/text_message.py
@@ -0,0 +1,24 @@
+info_text = '''
+🤖 MoodMaster Bot — Your Personal Mood Companion
+
+This intelligent bot uses OpenAI’s cutting-edge AI to:
+- Recommend music tailored to your current mood 🎵
+- Provide personalized motivational quotes to uplift your spirit 💬
+- Keep track of your mood history (coming soon!)
+
+Our goal is to be your digital companion, bringing positivity and motivation through every high and low.
+
+Powered by OpenAI & Python 🐍
+'''
+
+creator_text = '''
+Hi, I'm Aryan Puransanaye - the developer behind MoodMaster Bot.
+
+a Junior Python Developer and Python Instructor.
+
+Follow me on social media and Stay update with my latest projects.
+
+Email: aryanpuransanayeh@gmail.com
+
+let's connect! 🚀
+'''
